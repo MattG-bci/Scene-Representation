b939371deeb18837c21d34262fb3d8d70b178a97
MattG-bci <mateusz.f.grzybowski@gmail.com>
2023-01-11 17:41:35 +0000

    Following the Lightly AI tutorial on MoCo on Cifar-10 dataset

---
+++ src/tutorial.py
@@ -0,0 +1,99 @@
+import torch
+import torch.nn as nn
+import torchvision
+import pytorch_lightning as pl
+import copy
+import lightly
+
+from lightly.models.modules.heads import MoCoProjectionHead
+from lightly.models.utils import deactivate_requires_grad
+from lightly.models.utils import update_momentum
+from lightly.models.utils import batch_shuffle
+from lightly.models.utils import batch_unshuffle
+
+
+path_to_train = '../datasets/cifar10/train/'
+path_to_test = '../datasets/cifar10/test/'
+
+
+num_workers = 8
+batch_size = 512
+memory_bank_size = 4096
+seed = 1
+max_epochs = 100
+
+pl.seed_everything(seed)
+
+collate_fn = lightly.data.SimCLRCollateFunction(
+    input_size=32,
+    gaussian_blur=0.,
+)
+
+
+# Augmentations typically used to train on cifar-10
+train_classifier_transforms = torchvision.transforms.Compose([
+    torchvision.transforms.RandomCrop(32, padding=4),
+    torchvision.transforms.RandomHorizontalFlip(),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize(
+        mean=lightly.data.collate.imagenet_normalize['mean'],
+        std=lightly.data.collate.imagenet_normalize['std'],
+    )
+])
+
+# No additional augmentations for the test set
+test_transforms = torchvision.transforms.Compose([
+    torchvision.transforms.Resize((32, 32)),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize(
+        mean=lightly.data.collate.imagenet_normalize['mean'],
+        std=lightly.data.collate.imagenet_normalize['std'],
+    )
+])
+
+# We use the moco augmentations for training moco
+dataset_train_moco = lightly.data.LightlyDataset(
+    input_dir=path_to_train
+)
+
+# Since we also train a linear classifier on the pre-trained moco model we
+# reuse the test augmentations here (MoCo augmentations are very strong and
+# usually reduce accuracy of models which are not used for contrastive learning.
+# Our linear layer will be trained using cross entropy loss and labels provided
+# by the dataset. Therefore we chose light augmentations.)
+dataset_train_classifier = lightly.data.LightlyDataset(
+    input_dir=path_to_train,
+    transform=train_classifier_transforms
+)
+
+dataset_test = lightly.data.LightlyDataset(
+    input_dir=path_to_test,
+    transform=test_transforms
+)
+
+dataloader_train_moco = torch.utils.data.DataLoader(
+    dataset_train_moco,
+    batch_size=batch_size,
+    shuffle=True,
+    collate_fn=collate_fn,
+    drop_last=True,
+    num_workers=num_workers
+)
+
+dataloader_train_classifier = torch.utils.data.DataLoader(
+    dataset_train_classifier,
+    batch_size=batch_size,
+    shuffle=True,
+    drop_last=True,
+    num_workers=num_workers
+)
+
+dataloader_test = torch.utils.data.DataLoader(
+    dataset_test,
+    batch_size=batch_size,
+    shuffle=False,
+    drop_last=False,
+    num_workers=num_workers
+)
+
+
