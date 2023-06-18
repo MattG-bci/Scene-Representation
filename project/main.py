c585badf1a665e362aa60a5315568c50b0b78675
Ubuntu <ubuntu@ip-172-31-13-226.eu-west-1.compute.internal>
2023-06-18 21:19:04 +0000

    Implemented and run debugging on DINO architecture.

--- project/main.py
+++ project/main.py
@@ -1,32 +1,43 @@
 from multiprocessing.dummy import freeze_support
 from lightly.data.multi_view_collate import MultiViewCollate
 from lightly.transforms.simclr_transform import SimCLRTransform
-from utils.models.BYOL import *
+from utils.models.Dino import *
 from lightly.data import LightlyDataset
 from torchvision import transforms
+from src.load_data import CustomDataset
+import pandas as pd
+import warnings
 
-model = BYOL()
 
-transforms = SimCLRTransform(input_size=32)
+warnings.filterwarnings("ignore")
 
 
-cifar_path = "./datasets/"
-dataset = LightlyDataset(cifar_path, transform=transforms)
-collate_fn = MultiViewCollate()
+backbone = torchvision.models.resnet18() # for ResNet-50 there was an issue in memory allocation. Probably something to be optimised. 
+model = DINO(backbone)
+
+transforms = DINOTransform()
+
+
+df_train = pd.read_parquet("/home/efs/users/mateusz/Data-Mining/src/pq_labels/det_train_new.parquet", engine="fastparquet")
+df_val = pd.read_parquet("/home/efs/users/mateusz/Data-Mining/src/pq_labels/det_val_new.parquet", engine="fastparquet")
+df = pd.concat([df_train, df_val])
+images = df.name.values
+
+dataset = CustomDataset(images, transforms=transforms)
 
 dataloader = torch.utils.data.DataLoader(
     dataset,
     batch_size=256,
-    collate_fn=None,
-    shuffle=True,
+    shuffle=False,
     drop_last=True,
-    num_workers=8
+    num_workers=4
 )
 
+### Debugging the DINO architecture.
 if __name__ == "__main__":
     freeze_support()
-    accelerator = "mps" if torch.backends.mps.is_available() else "cpu"
+    accelerator = "cuda" if torch.cuda.is_available() else "cpu"
 
-    trainer = pl.Trainer(max_epochs=1, devices=1, accelerator=accelerator)
+    trainer = pl.Trainer(max_epochs=1, accelerator=accelerator, devices=1, fast_dev_run=True)
     trainer.fit(model=model, train_dataloaders=dataloader)
 
--- project/src/load_data.py
+++ project/src/load_data.py
@@ -1,34 +1,33 @@
-from data_structuring import *
+from .data_structuring import *
 from torch.utils.data import DataLoader, Dataset
 from sklearn.model_selection import train_test_split
+import os
+import numpy as np
+from PIL import Image
 
 
-df_train = pd.read_parquet("data_files/bdd100k_classificaton_train.parquet", engine="fastparquet")
-df_val = pd.read_parquet("data_files/bdd100k_classificaton_val.parquet", engine="fastparquet")
 
-X_train, X_test, y_train, y_test = train_test_split(df_train["name"].values, df_train[["weather", "scene", "timeofday"]].values, shuffle=True, random_state=1, stratify=df_train["weather"])
+#X_train, X_test, y_train, y_test = train_test_split(df_train["name"].values, df_train[["weather", "scene", "timeofday"]].values, shuffle=True, random_state=1, stratify=df_train["weather"])
    
 class CustomDataset(Dataset):
-    def __init__(self, imgs, labels):
+    def __init__(self, imgs, transforms):
         self.imgs = imgs
-        self.labels = labels
+        self.transforms = transforms
 
     def __len__(self):
         return len(self.imgs)
     
     def __getitem__(self, index):
-        root = "/Users/matt/Documents/Imperial College London/Modules/MSc - Scene Representation and Pre-Tagging/dataset/bdd100k/images/100k"
-        image = np.array(Image.open(os.path.join(root, "train", self.imgs[index])).convert("RGB"))
-        labels = self.labels[index]
-
-        return image, (labels[0], labels[1], labels[2])
+        image = Image.open(self.imgs[index]).convert("RGB")
+        image = self.transforms(image)
+        return image
 
 
-training_data = CustomDataset(X_test, y_test)
-f = open("data_files/tags.json")
-tags = json.load(f)
+#training_data = CustomDataset(X_test, y_test)
+#f = open("data_files/tags.json")
+#tags = json.load(f)
 
-for idx in range(10): # Visualising first 10 data points for testing.
-    img, labels = training_data[idx]
-    visualise_data_with_tags(img, labels, tags)
+#for idx in range(10): # Visualising first 10 data points for testing.
+#    img, labels = training_data[idx]
+#    visualise_data_with_tags(img, labels, tags)
 
