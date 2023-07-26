5f33b1fcc336d1bdda6f422eafd73db90eb4d38d
Ubuntu <ubuntu@ip-172-31-8-54.eu-west-1.compute.internal>
2023-07-26 10:29:12 +0000

    Added a prototype cross-attention to the prototype model for image and lidar features. Had to modify the PointNet dimensions to match with the ResNet50

--- project/utils/models/feature_extractors/PointNet.py
+++ project/utils/models/feature_extractors/PointNet.py
@@ -59,14 +59,14 @@ class PointNet(nn.Module):
     self.conv1 = nn.Conv1d(point_dim, 64, 1)
     self.conv2 = nn.Conv1d(64, 64, 1)
     self.conv3 = nn.Conv1d(64, 64, 1)
-    self.conv4 = nn.Conv1d(64, 128, 1)
-    self.conv5 = nn.Conv1d(128, 1024, 1)
+    self.conv4 = nn.Conv1d(64, 256, 1)
+    self.conv5 = nn.Conv1d(256, 2048, 1)
 
     self.bn1 = nn.BatchNorm1d(64)
     self.bn2 = nn.BatchNorm1d(64)
     self.bn3 = nn.BatchNorm1d(64)
-    self.bn4 = nn.BatchNorm1d(128)
-    self.bn5 = nn.BatchNorm1d(1024)
+    self.bn4 = nn.BatchNorm1d(256)
+    self.bn5 = nn.BatchNorm1d(2048)
 
   def forward(self, x):
     n_points = x.shape[1]
@@ -88,9 +88,9 @@ class PointNet(nn.Module):
     x = F.relu(self.bn5(self.conv5(x)))
     x = nn.MaxPool1d(n_points)(x)
 
-    x = x.view(-1, 1024)
+    x = x.view(-1, 2048)
     if self.return_local_features:
-      x = x.view(-1, 1024, 1).repeat(1, 1, n_points)
+      x = x.view(-1, 2048, 1).repeat(1, 1, n_points)
       return torch.cat([x.transpose(2, 1), local_point_features], 2), features_transformed
     else:
       return x, features_transformed
--- project/utils/models/prototype.py
+++ project/utils/models/prototype.py
@@ -1,6 +1,7 @@
 import copy
 from typing import Any
 import pytorch_lightning as pl
+from pytorch_lightning.utilities.types import STEP_OUTPUT
 import torch
 import torchvision
 from torch import nn
@@ -20,12 +21,44 @@ import torch.nn.functional as F
 from feature_extractors.PointNet import PointNet
 
 
-class CrossAttention(nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
-    
-    def forward(self, x):
-        pass
+class CrossAttentionModule(nn.Module):
+    def __init__(self, in_channels_query, in_channels_kv, out_channels, num_heads=8):
+        super(CrossAttentionModule, self).__init__()
+        # Define the dimension of the queries, keys, and values for both modalities
+        self.in_channels_query = in_channels_query
+        self.in_channels_kv = in_channels_kv
+        self.out_channels = out_channels
+        # Number of attention heads
+        self.num_heads = num_heads
+        # Linear projections for queries, keys, and values
+        self.query_projection = nn.Linear(in_channels_query, out_channels)
+        self.key_projection = nn.Linear(in_channels_kv, out_channels)
+        self.value_projection = nn.Linear(in_channels_kv, out_channels)
+        # Scaled Dot-Product Attention layer
+        self.softmax = nn.Softmax(dim=-1)
+        self.scale_factor = 1.0 / (out_channels // num_heads) ** 0.5
+        # Output projection
+        self.output_projection = nn.Linear(out_channels, out_channels)
+        
+    def forward(self, query, key, value):
+        # Project the queries, keys, and values
+        q = self.query_projection(query)
+        k = self.key_projection(key)
+        v = self.value_projection(value)
+        # Split the queries, keys, and values into multiple heads
+        q = q.view(*q.size()[:-1], self.num_heads, -1)
+        k = k.view(*k.size()[:-1], self.num_heads, -1)
+        v = v.view(*v.size()[:-1], self.num_heads, -1)
+        # Compute the attention scores and attention weights
+        scores = torch.matmul(q, k.transpose(-2, -1)) * self.scale_factor
+        attention_weights = self.softmax(scores)
+        # Apply attention to the values
+        attention_output = torch.matmul(attention_weights, v)
+        # Concatenate the attention outputs from all heads and reshape
+        attention_output = attention_output.view(*attention_output.size()[:-2], -1)
+        # Output projection
+        output = self.output_projection(attention_output)
+        return output
 
 
 class ProtoNet(pl.LightningModule):
@@ -33,14 +66,27 @@ class ProtoNet(pl.LightningModule):
         super().__init__()
         self.img_backbone = nn.Sequential(*list(img_backbone.children())[:-1])
         self.point_backbone = point_backbone
+        self.cross_attention_module = CrossAttentionModule(in_channels_query=1, in_channels_kv=1, out_channels=8)
 
     def forward(self, x):
-        img = x[0]
+        imgs = x[0]
         point_cloud = x[1]
-
+        vision_features = self.img_backbone(imgs)
+        vision_features = vision_features.squeeze(-2)
+        point_cloud_features, _ = self.point_backbone(point_cloud)
+        point_cloud_features = point_cloud_features.unsqueeze(-1)
+        out = self.cross_attention_module(vision_features, point_cloud_features, point_cloud_features)
+        return vision_features, point_cloud_features
+    
+    def training_step(self, batch, batch_idx):
+        vision_features, point_cloud_features = self.forward(batch)
 
 
 if __name__ == "__main__":
     img_backbone = torchvision.models.resnet50()
     point_backbone = PointNet(point_dim=3, return_local_features=False)
     model = ProtoNet(img_backbone, point_backbone)
+    point_cloud = torch.rand(2, 30000, 3)
+    imgs = torch.rand(2, 3, 224, 224)
+    x = (imgs, point_cloud)
+    model(x)
