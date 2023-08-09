66b9ce9574af75bbb409aa9588ba6a23219d8da4
Ubuntu <ubuntu@ip-172-31-8-54.eu-west-1.compute.internal>
2023-08-09 13:46:00 +0000

    Added ResNet-50 backbone trained with DINO

--- project/src/visualise_embeddings.py
+++ project/src/visualise_embeddings.py
@@ -14,10 +14,14 @@ from utils.models.SSL.DINO import *
 
 warnings.filterwarnings("ignore")
 
-def load_backbone(checkpoint_path, model_class, backbone):
-    model = model_class.load_from_checkpoint(checkpoint_path, backbone=backbone)
-    backbone = model._modules[backbone]
-    return backbone
+def load_backbone(checkpoint_path, model_class, backbone_model, target):
+    model = model_class.load_from_checkpoint(checkpoint_path, backbone=backbone_model)
+    backbone = model._modules[target]
+    state_dict = {}
+    for (key, val), (k, v) in zip(backbone_model.state_dict().items(), backbone.state_dict().items()):
+        state_dict[key] = v
+    backbone_model.load_state_dict(state_dict)
+    return backbone_model
 
 def save_features(features):
     df = pd.DataFrame(data=features)
@@ -50,27 +54,30 @@ def plot_embeddings(network, dataloader, batch_size, save_components=True):
 if __name__ == "__main__":
     torch.manual_seed(1)
     torch.set_grad_enabled(False)
-    checkpoint_path = "/home/ubuntu/users/mateusz/Scene-Representation/project/tb_logs/DINOv1 - NuScenes/version_9/checkpoints/epoch=19-step=4400.ckpt"
-    student_network = load_backbone(checkpoint_path, DINO, "student_backbone")
+    backbone = torchvision.models.resnet50()
+    backbone.fc = nn.Sequential()
+    checkpoint_path = "/home/ubuntu/users/mateusz/Scene-Representation/project/tb_logs/DINOv1 - NuScenes/multi-view + colorjitter + grayscale/checkpoints/epoch=40-step=9020.ckpt"
+    student_network = load_backbone(checkpoint_path, DINO, backbone, "student_backbone")
+    torch.save(student_network.state_dict(), "/home/ubuntu/users/mateusz/Scene-Representation/project/utils/models/backbones/weights/dino_rn50.pth")
 
-    data_root = "/home/efs/users/mateusz/data/nuscenes"#_tiny/v1.0-trainval"
-    SENSORS = ["CAM_FRONT", "CAM_BACK"]
-    transform = transforms.Compose([
-        transforms.PILToTensor(),
-        transforms.Resize(size=200),
-        transforms.CenterCrop(200),
-        transforms.ConvertImageDtype(torch.float32) 
-        ])
+    #data_root = "/home/efs/users/mateusz/data/nuscenes"#_tiny/v1.0-trainval"
+    #SENSORS = ["CAM_FRONT", "CAM_BACK"]
+    #transform = transforms.Compose([
+    #    transforms.PILToTensor(),
+    #    transforms.Resize(size=200),
+    #    transforms.CenterCrop(200),
+    #    transforms.ConvertImageDtype(torch.float32) 
+    #    ])
 
-    batch_size=2
-    val_dataset = NuScenesDataset(data_root, sensors=SENSORS, transform=transform, split="mini_val")
-    val_dataloader = torch.utils.data.DataLoader(
-        val_dataset,
-        batch_size=batch_size,
-        shuffle=False,
-        drop_last=False,
-        num_workers=4
-    )
+    #batch_size=2
+    #val_dataset = NuScenesDataset(data_root, sensors=SENSORS, transform=transform, split="mini_val")
+    #val_dataloader = torch.utils.data.DataLoader(
+    #    val_dataset,
+    #    batch_size=batch_size,
+    #    shuffle=False,
+    #    drop_last=False,
+    #    num_workers=4
+    #)
 
-    plot_embeddings(student_network, val_dataloader, batch_size, save_components=False)
+    #plot_embeddings(student_network, val_dataloader, batch_size, save_components=False)
 
