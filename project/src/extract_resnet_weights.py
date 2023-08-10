9fcba428320f40a4d19b945e4ceb72699cfd18ac
Ubuntu <ubuntu@ip-172-31-8-54.eu-west-1.compute.internal>
2023-08-10 10:20:11 +0000

    Added ResNet50 weights trained on KITTI 3D Object Detection

--- project/src/extract_resnet_weights.py
+++ project/src/extract_resnet_weights.py
@@ -2,26 +2,14 @@ import torch
 import torchvision
 import pickle
 
-def extract_backbone_weights(path):
-    model = torch.load(path)
+def extract_weights(path, model):
+    loaded_state_dict = torch.load(path)
     new_state_dict = {}
-    for name, weight in model["state_dict"].items():
-        if "backbone" in name:
-            name = ".".join(name.split(".")[1:])
-            new_state_dict[name] = weight
-
+    for (key, val), (key_pickle, val_pickle) in zip(model.state_dict().items(), loaded_state_dict.items()):
+        new_state_dict[key] = val_pickle
     return new_state_dict
 
-def extract_from_pickle(path):
-    file = open(path, "rb")
-    data = pickle.load(file)
-    new_state_dict = {}
-    for name, weight in data.items():
-        new_name = name.replace("model.depth_net.encoder.encoder.", "")
-        new_state_dict[new_name] = weight
-    return new_state_dict
-
-def extract_features_from_pickle(path, model):
+def extract_weights_from_pickle(path, model):
     file = open(path, "rb")
     data = pickle.load(file)
     new_state_dict = {}
@@ -30,11 +18,11 @@ def extract_features_from_pickle(path, model):
     return new_state_dict
 
 if __name__ == "__main__":
-    path =  "/home/ubuntu/users/mateusz/Scene-Representation/project/utils/models/backbones/weights/carnet_depth_resnet50.pickle"
-    state_dict = extract_from_pickle(path)
-    model = torchvision.models.resnet18()
+    path =  "/home/ubuntu/users/mateusz/Scene-Representation/project/utils/models/backbones/weights/model_40000_pkl"
+    model = torchvision.models.resnet50()
     model.fc = torch.nn.Sequential()
-    state_dict = extract_features_from_pickle(path, model)
+    state_dict = extract_weights(path, model)
     model.load_state_dict(state_dict)
+    torch.save(model.state_dict(), "/home/ubuntu/users/mateusz/Scene-Representation/project/utils/models/backbones/weights/kitti_d4lcn_rn50.pth")
 
 
